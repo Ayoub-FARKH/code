192567cc3a327afadf71334fb0eb9776e0c01673
farkh <persoayoub.farkh@gmail.com>
2025-09-11 22:52:33 +0200

    exo finis

--- ex42/c07/ex01/ft_range.c
+++ ex42/c07/ex01/ft_range.c
@@ -17,22 +17,22 @@ int	*ft_range(int min, int max)
 	int	*arr;
 	int	i;
 
+	arr = 0;
 	if (min >= max)
 		return (NULL);
-	arr = malloc(sizeof(int) * (max - min));
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
 	i = 0;
-	while (min < max)
+	while (i < (max - min + 1))
 	{
-		arr[i] = min;
-		min++;
+		arr[i] = min + i;
 		i++;
 	}
 	return (arr);
 }
 
-/*
+
 #include <stdio.h>
 int	main(void)
 {
@@ -49,4 +49,3 @@ int	main(void)
 	free(arr);
 	return (0);
 }
-*/
